7517cb8a3962dbe4cf83934b0f1a2f18a92d0e82
kn-nisha <nisha.devops25@gmail.com>
2025-09-12 08:41:43 +0600

    Update main.jsx

--- run_mern_stack_with_docker_compose/frontend/src/main.jsx
+++ run_mern_stack_with_docker_compose/frontend/src/main.jsx
@@ -1,6 +1,30 @@
+Main configuration
+/*
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
+*/
+
+/*Added on 12th for generating stdout*/
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+
+// --- Redirect all console.error to stdout (so it shows in docker logs) ---
+const originalError = console.error;
+console.error = (...args) => {
+  console.log("🚨 FRONTEND ERROR:", ...args);
+  originalError(...args); // still keep default browser console
+};
+// ------------------------------------------------------------------------
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
